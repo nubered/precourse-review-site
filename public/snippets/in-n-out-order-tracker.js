76092c610178b313207d8220fda8efa73b79fba9
GrokN <FullGrok@gmail.com>
2017-05-07 14:45:29 -0700

    Flesh out Order class, implement driver code

--- public/snippets/in-n-out-order-tracker.js
+++ public/snippets/in-n-out-order-tracker.js
@@ -1,6 +1,29 @@
 class Order {
+  constructor() {
+    this.sequence = [
+      'Taking order',
+      'Cooking order',
+      'Request pickup of order',
+      'Order complete',
+    ]
 
+    this.state = 0 // set initial state to taking order
+  }
+
+  advanceState() { this.state++ }
+  getState() { return this.sequence[this.state] }
 }
+
+
+let order_1 = new Order
+let order_2 = new Order
+order_1.advanceState()
+order_1.advanceState()
+order_2.advanceState()
+order_1.advanceState()
+
+console.log('State of order 1 is ', order_1.getState())
+console.log('State of order 2 is ', order_2.getState())
 
 // create order instance #1
 // create order instance #2
