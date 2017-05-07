c86df8368476f57aae2d87514d5a89caa90d6a05
GrokN <FullGrok@gmail.com>
2017-05-07 15:08:25 -0700

    Refactor directory structure

--- public/snippets/clean-directory.sh
+++ public/snippets/clean-directory.sh
@@ -1,2 +1,12 @@
-# TODO: list your commands here, one per line.
+rm -rf server
+mkdir public public/css public/js
+mv client/style.css public/css
+mv client/bundle.js public/js
+mv client/index.html public
+touch index.js
+cd client
+rm multiplier.js
+mkdir js js/test
+mv app.js js
+mv test_multiplier.js js/test/multiplier.test.js
 
