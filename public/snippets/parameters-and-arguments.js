73daeb074ff2cf77210712a23b0035b3c45b9e9f
GrokN <FullGrok@gmail.com>
2017-05-07 13:47:02 -0700

    Make table header green

--- public/snippets/parameters-and-arguments.js
+++ public/snippets/parameters-and-arguments.js
@@ -2,4 +2,5 @@ const greenify = function(el) {
   el.setAttribute('style', 'background-color: green');
 };
 
-greenify();
+let tr = document.getElementsByTagName('thead')[0].getElementsByTagName('tr')[0]
+greenify(tr);
