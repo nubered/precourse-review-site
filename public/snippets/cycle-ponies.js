a14adbae527306266247dc01cfac23c2f727eca0
GrokN <FullGrok@gmail.com>
2017-05-07 20:47:56 -0700

    Cycle dem ponies

--- public/snippets/cycle-ponies.js
+++ public/snippets/cycle-ponies.js
@@ -1,1 +1,7 @@
-// your code here
+document.getElementById('btn-cycle-ponies')
+        .addEventListener(
+            'click',
+          () => {
+            let divPonies = document.getElementsByClassName('my-little-pony')
+            divPonies[0].parentNode.insertBefore(divPonies[1], divPonies[0])
+          })
